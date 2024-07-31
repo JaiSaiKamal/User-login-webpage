455e46641e492dba108c4ec227dc0bd5d9515344
JaiSaiKamal <130429660+JaiSaiKamal@users.noreply.github.com>
2024-07-31 09:25:41 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+
+var Schema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    gender:String,
+    status:String
+})
+
+const Userdb=mongoose.model('Userdb',Schema);
+
+module.exports=Userdb;
